37bcefb0568baae7f4961663a9a61d7205f192d3
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-05 15:35:36 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -4,6 +4,7 @@ from pyrogram.types import Chat, Message, User
 from config import BOT_USERNAME
 
 
-
-
-
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"RUKO ZARA SABAR KARO YAHA SPAM NA KARO JALDI JAO @ABHI_NETWORK 👈👈YAHA CLICK KARKE JOIN HO JAO WAHA BAT KARENGE
+  return
